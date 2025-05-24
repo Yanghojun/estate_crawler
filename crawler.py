b3f22756e1a3bcaa57d78ba225871ba68ec82dd9
yanghojun <hojune0303@naver.com>
2025-05-24 17:14:10 +0900

    add more code

--- crawler.py
+++ crawler.py
@@ -39,6 +39,74 @@ async def _start(data_url,
     except Exception as e:
         return f"예상치 못한 오류 발생: {e}"
 
+async def _transform_address(jiyeok: str) -> list:
+    client_id = os.environ.get('X_NCP_APIGW_API_KEY_ID')
+    client_pw = os.environ.get('X_NCP_APIGW_API_KEY')
+    naver_map_api_url = 'https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query='
+
+    add_lists = await _address_api(jiyeok)
+    result = set()
+
+    headers = {
+        'X-NCP-APIGW-API-KEY-ID': client_id,
+        'X-NCP-APIGW-API-KEY': client_pw
+    }
+
+    try:
+        async with httpx.AsyncClient(timeout=10.0) as client:
+            for add in add_lists:
+                add_urlenc = parse.quote(add)
+                url = naver_map_api_url + add_urlenc
+
+                resp = await client.get(url, headers=headers)
+                resp.raise_for_status()
+                response_body = resp.json()
+
+                if response_body.get('addresses'):
+                    sido = response_body['addresses'][0]['addressElements'][0]['shortName']
+                    result.add(sido)
+        return list(result)
+
+    except Exception as e:
+        return f"예상치 못한 오류 발생: {e}"
+
+
+async def _address_api(keyword,
+                    **kwargs):
+    urls = 'http://www.juso.go.kr/addrlink/addrLinkApi.do'
+    confmKey = os.environ.get('JUSO_API_KEY') # 필수 값 승인키
+    
+    params = {
+        'keyword': keyword,
+        'confmKey': confmKey,
+        'resultType': 'json'
+    }
+    
+    if kwargs: # 필수 값이 아닌 변수를 params에 추가
+        for key, value in kwargs.items():
+            params[key] = value
+    params_str = parse.urlencode(params) # dict를 파라미터에 맞는 포맷으로 변경
+    
+    url = '{}?{}'.format(urls, params_str)
+
+    try:
+        async with httpx.AsyncClient(timeout=10.0) as client:
+            response = await client.get(urls, params=params)
+            response.raise_for_status()
+            result = response.json()
+
+            status = result['results']['common']['errorMessage']
+            roadAddr_list = []
+
+            if status == '정상':
+                for juso in result['results']['juso']:
+                    roadAddr_list.append(juso['jibunAddr'])
+
+            return list(set(roadAddr_list))
+
+    except Exception as e:
+        return f"예상치 못한 오류 발생: {e}"
+
 @mcp.tool(
   name="get_result",
   description="대한민국의 아파트의 청약, 민간사전청약아파트, 민간임대오피스텔 등의 정보를 수집할 수 있는 tool입니다."
@@ -108,7 +176,20 @@ async def get_result(
     house_type_list = []
     jiyeok_list = []
 
-    return data_list
+    if jiyeok in enum_jiyeok:
+        jiyeok_list = [jiyeok]
+    else:
+        jiyeok = await _transform_address(jiyeok=jiyeok)        
+
+    if house_type != "전체":
+        h_type_key = type_keys[house_type]
+        house_type_list.extend(h_type_key)
+    if jiyeok != "전체" and not jiyeok_list:
+        for sido in jiyeok: 
+            jiyeok_key = jiyeok_keys[sido]
+            jiyeok_list.extend(jiyeok_key)
+
+    return jiyeok
 
 if __name__ == "__main__":
     print("MCP Server Start")
